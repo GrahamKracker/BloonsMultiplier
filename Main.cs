9bd5d37bf315435add0d42aece438105f2e95fcd
GrahamKracker <gjguzynski@gmail.com>
2023-05-20 11:39:56 -0400

    Initial Commit

---
+++ Main.cs
@@ -0,0 +1,33 @@
+﻿using BloonMultiplier;
+using BTD_Mod_Helper.Api.ModOptions;
+using Il2CppAssets.Scripts.Models;
+using Main = BloonsMultiplier.Main;
+
+[assembly: MelonInfo(typeof(Main), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+
+namespace BloonsMultiplier;
+
+[HarmonyPatch]
+public class Main : BloonsTD6Mod
+{
+    private static readonly ModSettingDouble Multiplier = new(10)
+    {
+        displayName = "Multiplier",
+        description = "The multiplier for all spawned bloons",
+        
+        minValue = 1,
+    };
+
+    public override void OnNewGameModel(GameModel result)
+    {
+        result.GetRoundSet().rounds.ForEach(round =>
+        {
+            round.groups.ForEach(group =>
+            {
+                var count = group.count * Multiplier;
+                group.count = (int)count;
+            });
+        });
+    }
+}
---
+++ ModHelperData.cs
@@ -0,0 +1,11 @@
+﻿namespace BloonMultiplier;
+
+public static class ModHelperData
+{
+    public const string Name = "BloonsMultiplier";
+    public const string Description = "Multiplies the number of bloons spawned by a configurable amount. Thanks to EngineerMonke#6025 for the icon";
+    public const string Version = "1.0.0";
+    public const string RepoOwner = "GrahamKracker";
+    public const string RepoName = "BloonsMultiplier";
+    public const string WorksOnVersion = "36.3";
+}
